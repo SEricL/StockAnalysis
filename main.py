827cb23e26fd042f34d059e963509dd7cefd876b
SEricL <eric_lan_2001@hotmail.com>
2023-05-11 20:08:13 -0400

    Linear Regression
    Currently extremely overtuned, look to add meaning/weights to dates

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import nltk
 import pandas as pd
 from sklearn import linear_model
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import accuracy_score
+
 import numpy as np
 import matplotlib as plt
 import yfinance as yf
@@ -17,10 +17,20 @@ tickerDf = tickerData.history(period='1d', start='2013-1-1', end='2023-5-1')
 
 # Drop missing rows, make target data frame
 tickerDf.dropna()
+tickerDf.drop('High', axis=1, inplace=True)
+tickerDf.drop('Low', axis=1, inplace=True)
+tickerDf.drop('Volume', axis=1, inplace=True)
 y = tickerDf['Close']
 tickerDf.drop('Close', axis=1, inplace=True)
 
 # Default test size is 30%, training set is 70%
-train, trainY, test, testY = train_test_split(tickerDf, y, test_size=0.3, random_state=42)
+trainX, testX, trainY, testY = train_test_split(tickerDf, y, test_size=0.3, random_state=42)
 linear = linear_model.LinearRegression()
-linear.fit(train, trainY)
+linear.fit(trainX, trainY)
+
+testFrame = tickerData.history(period='1d', start='2023-5-7', end='2023-5-11')
+testFrame.drop(columns=['High','Low', 'Volume','Close'], inplace=True)
+
+#prediction = linear.predict(testX)
+# print("Accuracy:", linear.score(testX, testY))
+print(linear.predict(testFrame))
